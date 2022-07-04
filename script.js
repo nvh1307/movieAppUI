23ba6a9fd50166be6228a6d0ee4baf6009907de7
Hung <nguyenviethung.13072000@gmail.com>
2022-07-04 16:54:55 +0700

    movieappUI

---
+++ script.js
@@ -0,0 +1,27 @@
+const APIURL = 'http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1';
+
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+
+async function getMovie() {
+    const resp = await fetch(APIURL);
+    const respData = await resp.json();
+    
+    respData.results.forEach((movie) => {
+        const {poster_path, title, vote_average} = movie
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+        movieEl.innerHTML = `
+        <img 
+            src="${IMGPATH + poster_path}"
+            alt="${title}"
+        />
+        <div class="movie-infor">
+            <h3>${title}</h3>
+            <span>${vote_average}</span>
+        </div>
+        `
+        document.body.appendChild(movieEl)
+    })
+    return respData
+}
+getMovie();
